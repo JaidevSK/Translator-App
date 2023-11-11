17b9b7650a683f16dbed5cfc0070bb2da8f56a98
JaidevSK <134712750+JaidevSK@users.noreply.github.com>
2023-11-11 20:42:48 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,29 +1,16 @@
 import streamlit as st
 from englisttohindi.englisttohindi import EngtoHindi
-from englisttohindi.englisttohindi import HinditoEng
+
+st.title("English to Hindi Translator")
 
 
-option = st.selectbox(
-    'Select Languages\nभाषा चुने',
-    ('English -> हिंदी', 'हिंदी -> English'))
+# message to be translated
+message = st.text_input("Enter the text to be translated from English to Hindi")
 
-if option == 'English -> हिंदी':
-  # message to be translated
-  message = st.text_input("Enter the text to be translated from English to Hindi")
-   
-  # creating a EngtoHindi() object
-  res = EngtoHindi(message)
-   
-  # displaying the translation
-  st.write(res.convert)
- 
-if option == 'हिंदी -> English':
-  # message to be translated
-  message = st.text_input("हिंदी से अंग्रेजी में अनुवादित होने वाला संदेश दर्ज करें")
-   
-  # creating a EngtoHindi() object
-  res = HinditoEng(message)
-   
-  # displaying the translation
-  st.write(res.convert)
+# creating a EngtoHindi() object
+res = EngtoHindi(message)
 
+# displaying the translation
+st.write("The translated text is:)
+st.divider() 
+st.write(res.convert)
